64c16710d46d1c040d175791008e9ebd569d82fc
Katie Campbell <ktcampbell241@gmail.com>
2017-06-01 10:58:49 -0700

    Update Factoring.java

--- Factoring.java
+++ Factoring.java
@@ -1,7 +1,12 @@
 public class Factoring{
-     public static void main{
+     public static void main(string[] args){
      //get integer
+          Scanner input = new Scanner(System.in);
+          System.out.print("Enter number to factor:");
+          double k = input.nextDouble();
+          
      //take square root
      //check for factors up to square root
+     //display results
      }
 }
