39fcc91fefb4ebc9578355e8957c982e1b26466e
Katie Campbell <ktcampbell241@gmail.com>
2017-06-01 11:04:04 -0700

    Update Factoring.java

--- Factoring.java
+++ Factoring.java
@@ -1,12 +1,13 @@
 public class Factoring{
      public static void main(string[] args){
-     //get integer
+          //get number to factor
           Scanner input = new Scanner(System.in);
           System.out.print("Enter number to factor:");
           double k = input.nextDouble();
           
-     //take square root
-     //check for factors up to square root
-     //display results
+               //take square root
+          //check for factors up to square root
+          //display results
+          System.out.println("The factorization for" + k + "is"/*result*/ );
      }
 }
